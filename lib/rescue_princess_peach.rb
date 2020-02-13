8e39cf2ac9e23141a4d38b24c7e1e015cddab0ff
Ryan Hollins <ballbaroi@gmail.com>
2020-02-13 15:16:04 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,6 +3,6 @@ def rescue_princess_peach
   puts phrase
 end
 ## Comment the below back in to see method scope in action!
-#rescue_princess_peach
-puts status
+# rescue_princess_peach
+# puts status
 
